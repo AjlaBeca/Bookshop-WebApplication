3273c03166912e4e40fce2c1c4e1badd755d8393
ajla <ajlabeca@gmail.com>
2024-06-01 22:38:26 +0200

    url fix

--- backend/public/v1/docs/swagger.php
+++ backend/public/v1/docs/swagger.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../../../vendor/autoload.php';
 if($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1'){
     define('BASE_URL', 'http://localhost/webprogramming_bookshop/backend/');
 } else {
-    define('BASE_URL', '/');
+    define('BASE_URL', 'https://oyster-app-yllp3.ondigitalocean.app/backend/');
 }
 
 error_reporting(0);
