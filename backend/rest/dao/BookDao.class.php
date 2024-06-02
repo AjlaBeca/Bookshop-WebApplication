e593983fd091486e6a4f9b6fff101aa85df17349
ajla <ajlabeca@gmail.com>
2024-06-02 18:03:09 +0200

    query fix

--- backend/rest/dao/BookDao.class.php
+++ backend/rest/dao/BookDao.class.php
@@ -11,11 +11,11 @@ class BookDao extends BaseDao {
         // Query to fetch all records from the specified table
         $query = "SELECT b.*, GROUP_CONCAT(t.Name) AS Tags, a.Name AS Author
           FROM book b
-          LEFT JOIN BookTag bt ON b.BookID = bt.BookID
+          LEFT JOIN booktag bt ON b.BookID = bt.BookID
           LEFT JOIN tag t ON bt.TagID = t.TagID
-          LEFT JOIN Author a ON b.AuthorID = a.AuthorID
-          LEFT JOIN Publisher p ON b.PublisherID = p.PublisherID
-          LEFT JOIN Genre g ON b.GenreID = g.GenreID
+          LEFT JOIN author a ON b.AuthorID = a.AuthorID
+          LEFT JOIN publisher p ON b.PublisherID = p.PublisherID
+          LEFT JOIN genre g ON b.GenreID = g.GenreID
           GROUP BY b.BookID";
 
     
