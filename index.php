14a22b4d6156b4a9a26a8f9106020c1a12666bbd
ajla <ajlabeca@gmail.com>
2024-06-03 17:39:39 +0200

    routing fix

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
-?>
+if (getenv('ENVIRONMENT') === "local") {
+    // If the application is running in a local environment, include '/webprogramming/index.html'
+    header("Location: /webprogramming_bookshop/login");
+} else {
+    // If the application is running in a deployed environment, include '/index.html'
+    header("Location: /login");
+}
+
+die();
