47d75e1184917683ee8df109c09f8c2260d12a2a
ajla <ajlabeca@gmail.com>
2024-06-03 17:51:32 +0200

    fix

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 if (getenv('ENVIRONMENT') === "local") {
     // If the application is running in a local environment, include '/webprogramming/index.html'
-    header("Location: /webprogramming_bookshop/login");
+    header("Location: /webprogramming_bookshop/index.html");
 } else {
     // If the application is running in a deployed environment, include '/index.html'
-    header("Location: /login");
+    header("Location: /index.html");
 }
 
 die();
